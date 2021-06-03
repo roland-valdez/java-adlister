cf93274b43a8fa7b3cbf704f4a013f03b6cc36b4
Roland Valdez <valdez.roland.23@gmail.com>
2021-06-03 12:05:06 -0500

    at a good startig point for #2

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,11 +1,15 @@
 package com.codeup.adlister.dao;
 
+
+import java.sql.SQLException;
+
 public class DaoFactory {
     private static Ads adsDao;
     private static Config config = new Config();
 
-    public static Ads getAdsDao() {
+    public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
+//            Config config = new Config();
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
